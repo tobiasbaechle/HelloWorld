570957a38135c26864762dbbfea17adea8f52e9a
Bachle.Tobias <Bachle.Tobias@SK-D43D7E37FA4C.2631.skool.net>
2017-09-20 16:54:05 +0200

    collatz folge fertig

--- HelloWorld/src/HelloWorld.java
+++ HelloWorld/src/HelloWorld.java
@@ -2,13 +2,21 @@
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		int n=19;
-	
+	 int n;
+	for(int i=700;i<=1000;i++){	
+		n=i;
+		System.out.print(n+": ");
+		while(n>1){
         if(n%2==0){
         n=n/2;
         }else{
         	n=3*n+1;
         }
-	    System.out.println(n);
-	}
+	    System.out.print(n+" ");
+		}
+	
+	System.out.println("");
+	}	
+}
+	
 }
